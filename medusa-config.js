6babf475471633431a2106609958632f14850ffa
bottle alime <phammanhtb2001@gmail.com>
2023-12-27 16:03:23 +0700

    install redis

--- medusa-config.js
+++ medusa-config.js
@@ -122,18 +122,18 @@ const modules = {
   stockLocationService: {
     resolve: "@medusajs/stock-location",
   },
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
-      redisUrl: REDIS_URL
-    }
+      redisUrl: REDIS_URL,
+    },
   },
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: REDIS_URL
-    }
-  },*/
+      redisUrl: REDIS_URL,
+    },
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
